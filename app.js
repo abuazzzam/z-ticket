1eab4af23cb2d713e1d9306c0035a7f89c997970
abuazzzam <wit.mahfuz@gmail.com>
2024-02-21 23:19:43 +0600

    Paribahan chart

--- app.js
+++ app.js
@@ -1,49 +1,107 @@
-const allBtn = document.getElementsByClassName("btns");
+const allSeats = document.getElementsByClassName("seats");
 
 let count = 20;
 
-for(const btn of allBtn){
-    btn.addEventListener("click", function(e){
-        count = count -1;
-        // document.getElementById("seatAvailable").innerText =count;
-
-//         const seatName = btn.innerHTML
-// console.log(seatName);
+for (const seat of allSeats) {
+    seat.addEventListener("click", function (e) {
+        count = count - 1;
+        
+        const seatName = e.target.parentNode.childNodes[1].innerText;
+        const category = 'Economy';
         const seatPrice = document.getElementById("price").innerText;
-        // console.log(seatPrice);
-
+        
         const seatCalculator = document.getElementById("seatCalculation");
 
-        const li = document.createElement("li");
-        
-        // const p = document.createElement("p");
-        // p.innerText = seatName;
+        const div = document.createElement("div");
+        // div.classList
+
         const p = document.createElement("p");
-        p.innerText = 'Economy';
+        p.innerText = seatName;
+        const p2 = document.createElement("p");
+        p.innerText = category;
         const p3 = document.createElement("p");
         p.innerText = seatPrice;
-
-        // li.appendChild(p);
-        li.appendChild(p);
-        li.appendChild(p3);
-        seatCalculator.appendChild(li);
-
-        const totalCost = document.getElementById("totalCost").innerText;
-        const convertedCost = parseInt(totalCost);
-        // document.getElementById("totalCost").innerText = convertedCost + parseInt(seatPrice);
         
-           const sum = convertedCost + parseInt(seatPrice);
+        div.appendChild(p);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        seatCalculator.appendChild(div);
+
+       
+        ttlCost("totalCost", parseInt(seatPrice));
+
+        gttlCost();
 
 
-
-        setInnerText ("totalCost", sum);
-        setInnerText ("seatAvailable", count);
         
+
+        
+        setInnerText("seatAvailable", count);
+
+
         
     });
 }
 
-function setInnerText (id, value){
+function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
 
 }
+
+function ttlCost (id, value){
+    const totalCost = document.getElementById(id).innerText;
+    const convertedCost = parseInt(totalCost);
+    // document.getElementById("totalCost").innerText = convertedCost + parseInt(seatPrice);
+
+    const sum = convertedCost + parseInt(value);
+    setInnerText("totalCost", sum);
+}
+
+// function gttlCost (id, value){
+//     const grandTotalCost = document.getElementById(id).innerText;
+//         const convertedGCost = parseInt(grandTotalCost);
+//         const sum2 = convertedGCost + parseInt(value);
+//         // console.log(grandTotalCost);
+
+//         setInnerText("gtotal", sum2);
+// }
+function gttlCost (status){ 
+    const totalCost = document.getElementById("totalCost").innerText;
+    let convertedCost = parseInt(totalCost);
+   
+    if(status == undefined){
+    
+        setInnerText("gtotal", convertedCost);
+    
+        // setInnerText("gtotal", convertedGCost);
+        // 
+    } else {
+        const inputValue = document.getElementById("input-field").value;
+        if(inputValue == "New15"){
+            setInnerText("gtotal", convertedCost = convertedCost * .85);
+        } 
+        else if(inputValue == "Couple20"){
+            setInnerText("gtotal", convertedCost = convertedCost * .80);
+        } 
+        
+        else {
+            alert('input correct coupon value');
+        }
+    } 
+    
+    
+        // setInnerText("gtotal", convertedGCost);
+
+    //     if(inputText ==='New15'){
+    //         setInnerText("gtotal", convertedGCost * .85);
+    //     }
+        // console.log(grandTotalCost);
+
+       
+}
+
+// function btnClick(){
+//     const inputValue = document.getElementById("input-field");
+//     const inputText = inputValue.value;
+//     console.log(inputText);
+// }
